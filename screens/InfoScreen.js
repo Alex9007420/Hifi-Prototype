f22e132a8271055563fbca3d63830162d1ba6338
jaforrer <jaforrer@student.ethz.ch>
2023-11-08 13:01:48 +0100

    fixed comment mistake

--- screens/InfoScreen.js
+++ screens/InfoScreen.js
@@ -13,13 +13,14 @@ const foodImages = [
   ];
 
 
-  export default function Infoscreen ({navigation}){ {/*You need to put this in the function if you want to navigate (everything you need to do to be able to navigate)*/}
+  export default function Infoscreen ({navigation}){ 
+  // You need to put this in the function if you want to navigate (everything you need to do to be able to navigate)
     return(
         <View style={styles.container}>
       {/* Search Bar Header */}
       <View style={styles.searchContainer}>
         <Pressable style={styles.searchContainer}
-        onPress={()=> navigation.navigate("Searchbar")}> {/*navigates you to the SearchBar*/}
+        onPress={()=> navigation.navigate("Searchbar")}> 
          <Text> Search for Recipes...</Text>
         </Pressable>
       </View>
