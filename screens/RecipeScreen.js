9d2d784e16cfdf43f5cdaa1bf0f17f8361c423a4
Michael Mörschell <mmorsche@gmail.com>
2023-11-15 00:10:33 +0100

    Add recipe details, create header style, fix indentation

--- screens/RecipeScreen.js
+++ screens/RecipeScreen.js
@@ -14,70 +14,72 @@ import IngredientsData from '../IngredientsData';
 
 export default function Recipe({route}){
   const Data = RecipeData.find((item) => item.id == route.params.id);
-  const iData = IngredientsData.filter((item) => Data.ingredients.includes(item.id));
-    return (
-        <ScrollView>
-           <Text >
-                {Data.name}
-            </Text>
-            <Image style={styles.imageItem} source={{ uri: Data.src}}/>
-        <Tools index={Data.id}/>
-        <Text >
-          Time:
-                {Data.time}
-        </Text>
-
-        {Data.ingredients.map((item) => (
-          <View>
-              <Text> {item}</Text>
-              <Ingredients category={item}/>
-          </View>
-              
-            ))}
-
-        </ScrollView>
-    )
+  // const iData = IngredientsData.filter((item) => Data.ingredients.includes(item.id));
+  return (
+    <ScrollView>
+      <Text style="recipeHeader">{Data.name}</Text>
+      <Image style={styles.imageItem} source={{ uri: Data.src}}/>
+      {
+        Data.details.map((item) => <Text>Detail: {item}</Text>)
+      }
+      <Tools index={Data.id}/>
+      <Text>Estimated preparation time: {Data.time}</Text>
+      { Data.ingredients.map((item) => (
+        <View>
+          <Text>{item}</Text>
+          <Ingredients category={item}/>
+        </View>
+      ))}
+    </ScrollView>
+  )
 }
+
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: 30,
-    },
-    searchContainer: {
-      padding: 10,
-      backgroundColor: '#fff',
-    },
-    searchInput: {
-      height: 40,
-      backgroundColor: '#f0f0f0',
-      borderRadius: 20,
-      paddingLeft: 10,
-    },
-    dashboard: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      padding: 5,
-    },
-    imageItem: {
-      height: 160,
-      margin: 5,
-      width: '45%', // Approximately 2 columns, depending on container width
-      borderRadius: 10,
-    },
-    footer: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      backgroundColor: '#fff',
-      borderTopWidth: 1,
-      borderTopColor: '#e1e1e1',
-      paddingVertical: 10,
-      paddingBottom: 20, // Padding to avoid overlap with home gesture
-    },
-    iconContainer: {
-      alignItems: 'center',
-    },
-    iconText: {
-      fontSize: 16,
-    },
-    });
+  recipeHeader: {
+    fontSize: 24,
+    color: 'red',
+    fontWeight: 'bold'
+  },
+  container: {
+    flex: 1,
+    marginTop: 30,
+  },
+  searchContainer: {
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  searchInput: {
+    height: 40,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 20,
+    paddingLeft: 10,
+  },
+  dashboard: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    padding: 5,
+  },
+  imageItem: {
+    height: 160,
+    margin: 5,
+    width: '45%', // Approximately 2 columns, depending on container width
+    borderRadius: 10,
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#fff',
+    borderTopWidth: 1,
+    borderTopColor: '#e1e1e1',
+    paddingVertical: 10,
+    paddingBottom: 20, // Padding to avoid overlap with home gesture
+  },
+  iconContainer: {
+    alignItems: 'center',
+  },
+  iconText: {
+    fontSize: 16,
+  },
+});
