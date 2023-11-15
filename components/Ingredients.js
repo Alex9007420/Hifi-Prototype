dbd85cc091d9e6b3bc1d8e8e2112e7d8cf957a91
jaforrer <jaforrer@student.ethz.ch>
2023-11-15 13:12:18 +0100

    created a centralised style file

--- components/Ingredients.js
+++ components/Ingredients.js
@@ -5,6 +5,7 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { FlatList } from 'react-native-gesture-handler';
 import RecipeData from '../data';
 import IngredientsData from '../IngredientsData';
+import styles from './Styles';
 
 export default function Ingredients({category}){
     const iData = IngredientsData.filter((item) => item.category == category);
@@ -35,48 +36,5 @@ const Ingredient = ({index}) =>{
         </View>
     );
 }
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: 30,
-      backgroundColor: '#f0f0f0',
-    },
-    searchContainer: {
-      padding: 10,
-      backgroundColor: '#fff',
-    },
-    searchInput: {
-      height: 40,
-      backgroundColor: '#f0f0f0',
-      borderRadius: 20,
-      paddingLeft: 10,
-    },
-    dashboard: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      padding: 5,
-    },
-    imageItem: {
-      height: 160,
-      margin: 5,
-      width: '45%', // Approximately 2 columns, depending on container width
-      borderRadius: 10,
-    },
-    footer: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      backgroundColor: '#fff',
-      borderTopWidth: 1,
-      borderTopColor: '#e1e1e1',
-      paddingVertical: 10,
-      paddingBottom: 20, // Padding to avoid overlap with home gesture
-    },
-    iconContainer: {
-      alignItems: 'center',
-    },
-    iconText: {
-      fontSize: 16,
-    },
-    });
+
 
---
+++ components/Styles.js
@@ -0,0 +1,47 @@
+import { StyleSheet, View, Text, TextInput, ScrollView, Image, TouchableOpacity, Pressable } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 30,
+      backgroundColor: '#f0f0f0',
+    },
+    searchContainer: {
+      padding: 10,
+      backgroundColor: '#fff',
+    },
+    searchInput: {
+      height: 40,
+      backgroundColor: '#f0f0f0',
+      borderRadius: 20,
+      paddingLeft: 10,
+    },
+    dashboard: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      padding: 5,
+    },
+    imageItem: {
+      height: 160,
+      margin: 5,
+      width: '45%', // Approximately 2 columns, depending on container width
+      borderRadius: 10,
+    },
+    footer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      backgroundColor: '#fff',
+      borderTopWidth: 1,
+      borderTopColor: '#e1e1e1',
+      paddingVertical: 10,
+      paddingBottom: 20, // Padding to avoid overlap with home gesture
+    },
+    iconContainer: {
+      alignItems: 'center',
+    },
+    iconText: {
+      fontSize: 16,
+    },
+    });
+    export default styles;
