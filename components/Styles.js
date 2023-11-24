4b6aace1d85b0956fc91f3887afdf24a06c8a11f
Mr-Vdv <vdv.mrc@gmail.com>
2023-11-24 15:03:36 +0100

    Styles of Cooking mode and centralised in styles.

--- components/Styles.js
+++ components/Styles.js
@@ -47,19 +47,6 @@ const styles = StyleSheet.create({
     iconText: {
       fontSize: 16,
     },
-    imageItem: {
-      height: 160,
-      margin: 5,
-      width: '97%', // Approximately 2 columns, depending on container width
-      borderRadius: 10,
-    },iconContainer: {
-      alignItems: 'center',
-    },
-    CookingMode: {
-        width: width, // important for flatlist
-        paddingToppadding: 0,
-        backgroundColor: '#080',
-    },
     imageContainer: {
         flex: 1, // important for flatlist
         width: width,
@@ -71,7 +58,7 @@ const styles = StyleSheet.create({
     CookingMode: {
       width: width, // important for flatlist
       paddingToppadding: 0,
-      backgroundColor: '#080',
+      backgroundColor: '#f0f0f0',
   },
   titel: {
     fontSize: 24,
@@ -90,6 +77,77 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     userSelect: 'text',
     padding: 2,
+  },
+  header: {
+    fontSize: 26,
+    margin: 20,
+    textAlign: 'center',
+  },
+  subheader: {
+    fontSize: 15,
+    margin: 20,
+    padding: 10,
+    textAlign: 'center',
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'black',
+  },
+  cookingModeButton: {
+    backgroundColor: 'red',
+    color: 'white',
+    width: width - 50,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderStyle: 'solid',
+    borderColor: '#cc0000',
+    margin: 25,
+    marginTop: 5,
+    padding: 12,
+  },
+  ingredientImageContainer: {
+    margin: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: '#bdf',
+    backgroundColor: '#fff',
+    shadowRadius: 5,
+    flex: 2,
+    flexDirection: 'column',
+  },
+  ingredientImage: {
+    width: '100%',
+    height: 200,
+    borderWidth: 1,
+    borderRadius: 12,
+    borderColor: '#eef',
+  },
+  ingredientHeading: {
+    marginTop: 15,
+    marginLeft: 1,
+    marginBottom: 10,
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  ingredientTextContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  ingredientText: { textAlign: 'right'},
+  ingredientPadding: {padding: 10},
+  stepContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginBottom: 10,
+  },
+  stepNumber: {
+    fontWeight: 'bold',
+    marginRight: 5,
+    marginLeft: 10,
+  },
+  stepText: {
+    flex: 1,
   },
     });
     export default styles;
