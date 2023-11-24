83f4acaf73e3bdf231bae87830cf708bc713cea7
Michael Mörschell <mmorsche@gmail.com>
2023-11-24 15:01:37 +0100

    (From Sergey) tasty new recipes

--- IngredientsData.js
+++ IngredientsData.js
@@ -328,7 +328,7 @@ const IngredientsData =[
         category: "Pizza topping",
         name: "Cippoli e pepperoni",
         ingredients: ["Onion", "Paprika", "Mozarella"],
-        picture: 'https://www.google.ru/url?sa=i&url=https%3A%2F%2Fwww.piccolericette.net%2Fpiccolericette%2Frecipe%2Fpizza-peperoni-e-prosciutto%2F&psig=AOvVaw1PuwiYPaPHYenZ9N46VGO_&ust=1700916437799000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCIDOzNbV3IIDFQAAAAAdAAAAABAD',
+        picture: 'https://i0.wp.com/www.piccolericette.net/piccolericette/wp-content/uploads/2016/07/3015_Pizza.jpg?zoom=2&resize=595%2C410&ssl=1',
         cookingstep: ["Lorem ipsum dolor sit amet", "consetetur sadipscing elitr", "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat", "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren", "no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet", "consetetur sadipscing elitr", "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat", "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren", "no sea takimata sanctus est Lorem ipsum dolor sit amet."],
 
     },
@@ -338,7 +338,7 @@ const IngredientsData =[
         category: "Pizza topping",
         name: "Napoletana",
         ingredients: ["Mozarella", "Anchovies"],
-        picture: 'https://www.google.ru/url?sa=i&url=https%3A%2F%2Fwww.saporinostri.it%2Fen%2Fle-origini-della-pizza-ricetta-pizza-verace-napoletana%2F&psig=AOvVaw0LcaroBVELO5PklORgJdtV&ust=1700916503367000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCIC_-vLV3IIDFQAAAAAdAAAAABAD',
+        picture: 'https://www.saporinostri.it/wp-content/uploads/2020/10/le-origini-della-pizza-napoletana-e1601990439881.jpg',
         cookingstep: ["Lorem ipsum dolor sit amet", "consetetur sadipscing elitr", "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat", "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren", "no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet", "consetetur sadipscing elitr", "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat", "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren", "no sea takimata sanctus est Lorem ipsum dolor sit amet."],
 
     },
@@ -351,5 +351,58 @@ const IngredientsData =[
         cookingstep: ["Lorem ipsum dolor sit amet", "consetetur sadipscing elitr", "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat", "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren", "no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet", "consetetur sadipscing elitr", "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat", "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren", "no sea takimata sanctus est Lorem ipsum dolor sit amet."],
 
     },
+    {
+        id: 40,
+        category: "Pizza topping",
+        name: "Al tonno",
+        ingredients: ["Tuna", "Mozarella", "Onion"],
+        picture: 'https://donluciano.pizza/125-large_default/pizza-tonno-e-cipolla.jpg',
+        cookingstep: ["Lorem ipsum dolor sit amet", "consetetur sadipscing elitr", "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat", "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren", "no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet", "consetetur sadipscing elitr", "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat", "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren", "no sea takimata sanctus est Lorem ipsum dolor sit amet."],
+
+    },
+    {
+        id: 41,
+        category: "Pizza sauce",
+        name: "Tomato",
+        ingredients: ["Olive oil 2tbsp", "Onion 1", "garlic clove", "Chopped tomatoes - 800g", "Tomato puree - 3 tbsp", "Dried oregano - 2 tbsp", "Brown sugar - 2 tbsp", "Basil - 1 small bunch"],
+        picture: 'https://joyfoodsunshine.com/wp-content/uploads/2018/09/best-homemade-pizza-sauce-recipe-1.jpg',
+        cookingstep: ["Heat the oil in a saucepan over a low heat, then add the onion along with a generous pinch of salt. Fry gently for 12-15 mins or until the onion has softened and is turning translucent. Add the garlic and fry for a further min. Tip in the tomatoes and purée along with the bay, oregano and sugar. Bring to the boil and lower the heat. Simmer, uncovered, for 30-35 mins or until thickened and reduced. Season. For a really smooth sauce, blitz with a stick blender, otherwise leave as is."],
+    },
+    {
+        id: 42,
+        category: "Pizza sauce",
+        name: "Spicy",
+        ingredients: ["Olive oil - 2 tbsp", "Onion", "garlic clove", "Chopped tomatoes - 800g", "Tomato puree - 3 tbsp", "Dried oregano - 2 tbsp", "Crushed red pepper extra hot - 1 tbsp", "Basil - 1 small bunch"],
+        picture: 'https://donluciano.pizza/125-large_default/pizza-tonno-e-cipolla.jpg',
+        cookingstep: ["Place tomato sauce, tomatoes, garlic, basil, oregano and crushed red pepper in large saucepan. Season with salt and pepper, bring to a boil over medium-high heat. Reduce heat and simmer, allowing flavors to combine."],
+    },
+    {
+        id: 43,
+        category: "Pizza sauce",
+        name: "BBQ",
+        ingredients: ["Ketchup - 250mk", "Brown sugar 5 tbsp", "Apple cider vinegar - 3 tbsp", "Worcester sauce - 1 tsp", "Mustard powder - 1/4 tsp", "Onion powder - 1/4 tsp"],
+        picture: 'https://i0.wp.com/tableoflaughter.com/wp-content/uploads/2022/05/bbq-pizza-sauce-spoon-7.jpg?w=1200&ssl=1',
+        cookingstep: ["Combine all ingredients together in a small saucepan, on medium-low heat. Whisk everything together until it is all well combined and bring the mixture to a simmer. Partially cover the saucepan and let the sauce simmer for 10-15 minutes, stirring occasionally, until it has thickened. Let the sauce cool to room temperature before placing in the fridge to cool completely.Use as desired on pizzas."],
+
+    },
+    {
+        id: 44,
+        category: "Pizza dough",
+        name: "Traditional italian",
+        ingredients: ["Warm water - 400 ml", "Flour - 1kg", "Fresh yeast - 3 tbsp", "Olive oil - 6 tbsp", "Salt - 1.5 tsp", "Sugar - 2 tsp"],
+        picture: 'https://www.nonnabox.com/wp-content/uploads/Pizza-Dough-Bread-83141-800x400.jpg',
+        cookingstep: ["Dump your flour, salt, and sugar into a bowl and mix them to get air-in helping break-up clumps the flour. Then- on a silicone pastry sheet carefully dump the flour onto the sheet forming a little mountain. Make a hole at the top (about fist-sized) for the water mixture to go in. Slowly pour the water mixture into the top of the flour pile.As this begins to wet-out the flour, then go to your original oil bowl. Take the flour bowl you used previously and with a pastry brush swab the remaining oil and wet-out the flour bowl. Once or twice is good. The flour dust will help the oil to stick to the bowl better. This is used later to put for your finished dough- now it won’t stick. Then back to your flour and water mixture, start to mix in the remaining flour and get it blended until it starts to become a dough-like texture. This is when you can begin kneading it to make it smoother and more refined. Once you have it elastic enough, it should be rolled into a ball. Use a light dusting of flour nearby to keep your hands from sticking too much. Put the ball into the oiled bowl and give it a couple spins to coat the ball. Now you cover the bowl and let it sit. It will need 4-5 hours at room temperature to be truly authentic!"],
+
+    },
+    {
+        id: 45,
+        category: "Pizza dough",
+        name: "Fluffy",
+        ingredients: ["Warm water - 400ml", "Yeast - 1 tbsp", "Sugar - 1 tbsp", "Salt - 1 tsp", "Olive oil - 50ml", "Flour - 6 cups"],
+        picture: 'https://www.withspice.com/wp-content/uploads/2020/07/chewy-pizza-dough-recipe-with-big-bubbles-in-crust-683x1024.jpg.webp',
+        cookingstep: ["Pour your yeast-water mixture into a large mixing bowl and then slowly add the first half of the flour. This mixture will be like pancake batter, so then begin to add the rest of the flour to thicken it up. When you can’t stir anymore it’s ready for kneading. Scrape the dough onto your pastry matt that is dusted with left-over flour. You might need to add more flour to control the stickiness. When the dough is nice and smooth you can then put it aside to let it rise. The bowl you used for flour can work if it’s all empty and cover with a kitchen towel. Be sure to let it sit in a warm area for one hour to have the maximum fluffiness you are looking for. Even up to 2 hours is always better for a very puffy crust. It depends on the warmth of the room too so 72-75F is best."],
+
+    },
+
     ];
 export default IngredientsData;
--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const RecipeData =[
     time: "30",
     src: 'https://images.prismic.io/eataly-us/ed3fcec7-7994-426d-a5e4-a24be5a95afd_pizza-recipe-main.jpg?auto=compress,format',
     tools: ["blender", "beaker", "stove", "trash-can", "microwave", "kettle", "grill", "pot-mix", "pot-steam", "chef-hat"],
-    ingredients: ["Dough", "Sauce", "Pizza topping"],
+    ingredients: ["Pizza dough", "Pizza sauce", "Pizza topping"],
   },
   {
     id: "2",
